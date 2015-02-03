0ff963d1b5a5e806d3f7855a6d3112e56b93e575
CoryWilson <ctwilson08@gmail.com>
2015-02-02 19:32:12 -0500

    form.html --> day1.php

--- day1.php
+++ day1.php
@@ -1,8 +1,22 @@
 <?
-//phpinfo(); single line comment
-/*multiline comment*/
+  //phpinfo(); single line comment
+  /*multiline comment*/
 
-  $name = "joe";
+  //$name = "joe";
 
+  //echo 'My name is '.$name.' , hi';
+
+  //$myarr = array("name"=>"mike");
+
+  // function get(){
+  //   echo "inside get";
+  // }
+
+  //get();
+  //var_dump($name);
+  //var_dump($myarr);
+  //var_dump(get());
+
+  var_dump($_POST);
 
 ?>
