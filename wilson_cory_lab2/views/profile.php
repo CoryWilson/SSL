25fb8f13de8e6bb254a0e20de9e06a7efd518e37
CoryWilson <ctwilson08@gmail.com>
2015-02-06 12:57:55 -0500

    profile.php fixed
    var_dump removed

--- wilson_cory_lab2/views/profile.php
+++ wilson_cory_lab2/views/profile.php
@@ -2,6 +2,5 @@
   <h3>Hello, <? echo $par2["username"];?>!</h3>
 </div>
 <div class="row">
-  <? var_dump($par2) ?>
   <h3>Your hashed password is: <h4><? echo sha1($par2["password"]);?></h4></h3>
 </div>
